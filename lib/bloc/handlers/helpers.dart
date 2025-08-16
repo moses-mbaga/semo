faceba99b371bb67ad58e978ee296e8b13555937
Moses Mbaga <codeloperstz@gmail.com>
2025-08-16 19:50:11 +0300

    Refactor

--- lib/bloc/handlers/helpers.dart
+++ lib/bloc/handlers/helpers.dart
@@ -10,7 +10,7 @@ class HandlerHelpers {
   final Logger _logger = Logger();
   final TMDBService _tmdbService = TMDBService();
 
-  Future<Movie> fetchMovieById(AppState state, int id) async {
+  Future<Movie?> fetchMovieById(AppState state, int id) async {
     if (state.movies != null && state.movies!.any((Movie movie) => movie.id == id)) {
       return state.movies!.firstWhere((Movie movie) => movie.id == id);
     } else if (state.incompleteMovies != null && state.incompleteMovies!.any((Movie movie) => movie.id == id)) {
@@ -25,9 +25,10 @@ class HandlerHelpers {
         }
       } catch (e, s) {
         _logger.e("Error fetching movie with ID $id", error: e, stackTrace: s);
-        rethrow;
       }
     }
+
+    return null;
   }
 
   Future<List<Movie>> fetchMoviesByIds(AppState state, List<int> ids) async {
@@ -35,15 +36,17 @@ class HandlerHelpers {
 
     for (final int id in ids) {
       try {
-        Movie movie = await fetchMovieById(state, id);
-        movies.add(movie);
+        Movie? movie = await fetchMovieById(state, id);
+        if (movie != null) {
+          movies.add(movie);
+        }
       } catch (_) {}
     }
 
     return movies;
   }
 
-  Future<TvShow> fetchTvShowById(AppState state, int id) async {
+  Future<TvShow?> fetchTvShowById(AppState state, int id) async {
     if (state.tvShows != null && state.tvShows!.any((TvShow tvShow) => tvShow.id == id)) {
       return state.tvShows!.firstWhere((TvShow tvShow) => tvShow.id == id);
     } else if (state.incompleteTvShows != null && state.incompleteTvShows!.any((TvShow tvShow) => tvShow.id == id)) {
@@ -58,9 +61,10 @@ class HandlerHelpers {
         }
       } catch (e, s) {
         _logger.e("Error fetching TV Show with ID $id", error: e, stackTrace: s);
-        rethrow;
       }
     }
+
+    return null;
   }
 
   Future<List<TvShow>> fetchTvShowsByIds(AppState state, List<int> ids) async {
@@ -68,8 +72,10 @@ class HandlerHelpers {
 
     for (final int id in ids) {
       try {
-        TvShow tvShow = await fetchTvShowById(state, id);
-        tvShows.add(tvShow);
+        TvShow? tvShow = await fetchTvShowById(state, id);
+        if (tvShow != null) {
+          tvShows.add(tvShow);
+        }
       } catch (_) {}
     }
 
