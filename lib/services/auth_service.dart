345cb0b13d87dea8eb0c6757fbed0f18717347a7
Moses Mbaga <codeloperstz@gmail.com>
2025-08-16 14:59:18 +0300

    Refactor

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -19,8 +19,9 @@ class AuthService {
       return GoogleAuthProvider.credential(idToken: auth.idToken);
     } catch (e, s) {
       _logger.e("Failed to get OAuth Credential", error: e, stackTrace: s);
-      rethrow;
     }
+
+    return null;
   }
 
   Future<UserCredential?> signIn() async {
@@ -34,8 +35,9 @@ class AuthService {
       return await _auth.signInWithCredential(credential);
     } catch (e, s) {
       _logger.e("Failed to authenticate", error: e, stackTrace: s);
-      rethrow;
     }
+
+    return null;
   }
 
   bool isAuthenticated() {
@@ -44,8 +46,9 @@ class AuthService {
       return user != null;
     } catch (e, s) {
       _logger.e("Failed to check authentication status", error: e, stackTrace: s);
-      rethrow;
     }
+
+    return false;
   }
 
   User? getUser() {
@@ -53,8 +56,9 @@ class AuthService {
       return _auth.currentUser;
     } catch (e, s) {
       _logger.e("Failed to get current user", error: e, stackTrace: s);
-      rethrow;
     }
+
+    return null;
   }
 
   Future<UserCredential?> reAuthenticate() async {
@@ -72,21 +76,21 @@ class AuthService {
       return await _auth.currentUser?.reauthenticateWithCredential(credential);
     } catch (e, s) {
       _logger.e("Failed to re-authenticate", error: e, stackTrace: s);
-      rethrow;
     }
+
+    return null;
   }
 
-  Future<dynamic> signOut() async {
+  Future<void> signOut() async {
     try {
       await _googleSignIn.signOut();
       await _auth.signOut();
     } catch (e, s) {
       _logger.e("Failed to sign out", error: e, stackTrace: s);
-      rethrow;
     }
   }
 
-  Future<dynamic> deleteAccount() async {
+  Future<void> deleteAccount() async {
     try {
       if (_auth.currentUser == null) {
         throw Exception("User is null");
@@ -95,7 +99,6 @@ class AuthService {
       await _auth.currentUser?.delete();
     } catch (e, s) {
       _logger.e("Failed to delete account", error: e, stackTrace: s);
-      rethrow;
     }
   }
 }
