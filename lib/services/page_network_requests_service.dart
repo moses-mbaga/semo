e96306a611df044b51f46b261e00a31ab4676a7b
Moses Mbaga <codeloperstz@gmail.com>
2025-09-11 01:40:50 +0300

    Add user agent

--- lib/services/page_network_requests_service.dart
+++ lib/services/page_network_requests_service.dart
@@ -95,6 +95,7 @@ class PageNetworkRequestsService {
           useShouldInterceptRequest: true,
           useOnLoadResource: true,
           mediaPlaybackRequiresUserGesture: false,
+          userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
         );
 
     final String snifferJs = _buildSnifferJs(includePatterns, _defaultHandlerName);
