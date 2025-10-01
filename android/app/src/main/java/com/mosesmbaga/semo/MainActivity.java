f10443e45de91363150f2847b7700448d3df48bf
Moses Mbaga <codeloperstz@gmail.com>
2025-10-01 16:35:55 +0300

    Flag app as media app

--- android/app/src/main/java/com/mosesmbaga/semo/MainActivity.java
+++ android/app/src/main/java/com/mosesmbaga/semo/MainActivity.java
@@ -1,6 +1,12 @@
 package com.mosesmbaga.semo;
 
+import android.media.AudioManager;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+    }
 }
